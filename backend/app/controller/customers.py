deae3a1b32c61a729ba5b0e6fa71a8d7b6068e68
mboma99 <jamesmboma99@googlemail.com>
2024-01-28 12:47:41 +0000

    Your commit message

--- backend/app/controller/customers.py
+++ backend/app/controller/customers.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter, Depends, Security
-
+from fastapi import HTTPException
 from backend.app.service.schema import ResponseSchema
 from backend.app.repository.auth_repo import JWTBearer, JWTRepo
-
 from fastapi.security import HTTPAuthorizationCredentials
 from backend.app.service.customers import CustomerService
 
@@ -18,3 +17,14 @@ async def get_user_profile(credentials: HTTPAuthorizationCredentials = Security(
     token = JWTRepo.extract_token(credentials)
     result = await CustomerService.get_customer_profile(token['email'])
     return ResponseSchema(detail="Successfully fetch data!", result=result)
+
+
+
+@router.get("/logged_in", response_model=ResponseSchema, response_model_exclude_none=True)
+async def is_user_logged_in(credentials: HTTPAuthorizationCredentials = Security(JWTBearer())):
+    token = JWTRepo.extract_token(credentials)
+    is_valid = await CustomerService.get_customer_profile(token['email'])
+    if is_valid is not None:
+        return ResponseSchema(detail="Logged In", result=True)
+    else:
+        raise HTTPException(status_code=401, detail="Invalid token or expired", result=False)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import Footer from './components/navigation/Footer';
 import Background from './components/backgrounds/Background';
 import BackgroundActions from './components/backgrounds/BackgroundActions';
 
-
 import Navbar from './components/navigation/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -15,30 +14,42 @@ import { Shop } from './pages/Shop';
 import { Cart } from './pages/Cart';
 import RegisterForm from './pages/RegisterForm';
 import ForgotPassword from './pages/ForgotPassword';
+import CustomerAccount from './components/customer/CustomerAccount';
 
 function App() {
-  
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    const auth = localStorage.getItem('auth_token');
+    setToken(auth);
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="relative overflow-hidden bg-[#1B223C] font-outfit">
         <Background />
         <Navbar />
         <div className="flex flex-col max-w-screen-xl mx-auto relative z-10">
-          <div className='flex-grow'>
+          <div className="flex-grow">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/shop" element={<Shop />} />
-              <Route path='/cart' element={<Cart />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='forgot-password' element={<ForgotPassword />} />
-              <Route path='/register' element={<RegisterForm />} />
+              <Route path="/cart" element={<Cart />} />
+              {token !== null && token !== undefined ? (
+                <Route path="/account" element={<CustomerAccount />} />
+              ) : (
+                <Route path="/login" element={<Login />} />
+              )}
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/register" element={<RegisterForm />} />
             </Routes>
           </div>
         </div>
         <Footer />
       </div>
     </BrowserRouter>
-  )}
+  );
+}
 
 export default App;
